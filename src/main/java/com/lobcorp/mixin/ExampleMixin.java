dc352599457f79dfdb93d2b741fde23f84ddaea5
DT <dt92203@gmail.com>
2024-08-06 15:47:51 -0700

    Working on mixins

--- src/main/java/com/lobcorp/mixin/ExampleMixin.java
+++ src/main/java/com/lobcorp/mixin/ExampleMixin.java
@@ -1,15 +1,17 @@
 package com.lobcorp.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.lobcorp.LobotomyCorporation121;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
+
+
+@Mixin(LivingEntity.class)
+public abstract class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/entity/LivingEntity;takeKnockback(DDD)V")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
 }
